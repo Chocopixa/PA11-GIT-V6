f6217aa690550116a066922d17ab1a138a75386c
Chocopixa <benben2212@outlook.com>
2023-11-06 05:14:35 +0800

    did lab 10 lol

---
+++ 27_benedict_LS10/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameManager Instance;
+    public static int score;
+    public Text scoretxt;
+    public GameObject GameOverCanvas;
+    public static bool hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+        hit = false;
+        GameOverCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hit != true)
+        {
+            updatescore();
+        }
+        if (hit == true && GameOverCanvas.activeSelf != true)
+        {
+            Gameover();
+        }
+    }
+    public static void AddScore(int ScoreToAdd)
+    {
+        score += ScoreToAdd;
+    }
+    public void Gameover()
+    {
+        GameOverCanvas.SetActive(true);
+    }
+    public void updatescore()
+    {
+        scoretxt.text = "Score: " + score.ToString();
+    }
+}
---
+++ 27_benedict_LS10/Assets/Scripts/ObstacleMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour
+{
+    private float xSpeed = -4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(xSpeed * Time.deltaTime, 0, 0f));
+        if (transform.position.x < -10)
+        {
+            GameManager.AddScore(1);
+            Destroy(this.gameObject);
+        }
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameManager.hit = true;
+            Destroy(this.gameObject);
+        }
+    }
+}
